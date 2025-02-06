4fba3cacea76f151b191cfb6793e065bcb73d85d
Hp <Hp@192.168.1.5>
2025-02-06 14:20:55 +0530

    commit 1

---
+++ src/demo/Additionofarrayno2.java
@@ -0,0 +1,27 @@
+package demo;
+
+public class Additionofarrayno2 {
+
+	public static void main(String[] args) {
+	
+		  int[] i = {1, 2, 3, 4, 5}; // Array of integers
+	        int sum = 0;                // Variable to store the sum
+	        
+	        // Loop through the array and add the elements
+	        //for (int num : i) {
+	          //  sum += num;
+	       // }
+	        
+	        // Print the result
+	        //System.out.println("The sum of the array elements is: " + sum);
+	        for(int j=0; j<i.length;j++) {
+	        	sum=sum+i[j];
+	        	
+	        }
+	        System.out.println(sum);
+    }
+	    }
+	
+	
+
+
